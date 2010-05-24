8fb49c827fdd3d4673c227f5254f955214984138
Joe Chrysler <mail@joechrysler.com>
2010-05-24 14:20:50 -0400

    Fixed indentation on driver

--- getPerson.php
+++ getPerson.php
@@ -8,35 +8,35 @@ require_once 'php/LDAP.class.php';
 //  Transient Data
 // ----------------
 
-   // Database Connections
-   $MySQL = new DataBase($db_host, $db_name);
-   $LDAP = new LDAP($ldap_url);
+// Database Connections
+	$MySQL = new DataBase($db_host, $db_name);
+	$LDAP = new LDAP($ldap_url);
 
-   // Container for the person being looked up
-   $Person = new Person();
-   $Person->setID(trim($_GET['id']));
+// Container for the person being looked up
+	$Person = new Person();
+	$Person->setID(trim($_GET['id']));
 
-   // Lists of which database fields the user can see
-   $AuthorizedLDAPFields = array();
-   $AuthorizedMySQLFields = '';
+// Lists of which database fields the user can see
+	$AuthorizedLDAPFields = array();
+	$AuthorizedMySQLFields = '';
 
-   // Temporary storage for info on the person being looked up
-   // ingested into the Person object during the import phase
-   $LDAPRecord = array();
-   $MySQLRecord = array();
-   $SupportHistory = array();
-   $EligibleForSoftwareCheckout = false;
-   $username = '';
+// Temporary storage for info on the person being looked up
+// ingested into the Person object during the import phase
+	$LDAPRecord = array();
+	$MySQLRecord = array();
+	$SupportHistory = array();
+	$EligibleForSoftwareCheckout = false;
+	$username = '';
 
-   // Information about the user that is currently accessing xtac
-   // The role which the active user fulfills (library, admin, intern, etc.)
-   // fetched from the MySQL database
-   $AuthorizedUsername = $_SERVER['PHP_AUTH_USER'];
-   $AuthorizationLevel = '';
+// Information about the user that is currently accessing xtac
+// The role which the active user fulfills (library, admin, intern, etc.)
+// fetched from the MySQL database
+	$AuthorizedUsername = $_SERVER['PHP_AUTH_USER'];
+	$AuthorizationLevel = '';
 
-   // A list of all the facts that the university should know about someone
-   // used to build the Person object during the import phase.
-   $PersonalAttributes = array();
+// A list of all the facts that the university should know about someone
+// used to build the Person object during the import phase.
+	$PersonalAttributes = array();
 
 
 
@@ -45,22 +45,22 @@ require_once 'php/LDAP.class.php';
 //  Get raw data from both Databases
 // ----------------------------------
 
-   $MySQL
-      ->connect($db_user, $db_pass)
-      ->getRole($AuthorizedUsername, $AuthorizationLevel)
-      ->getAuthorizedFields($AuthorizedUsername, $AuthorizedMySQLFields, $AuthorizedLDAPFields)
-      ->getUsername($Person->id, $username)
-      ->getUser($Person->id, $AuthorizedMySQLFields, $MySQLRecord)
-      ->getHistory($Person->id, $SupportHistory)
-      ->checkMSEligibility($Person->id, $EligibleForSoftwareCheckout)
-      ->getAttributes($PersonalAttributes)
-      ->disconnect();
+	$MySQL
+		->connect($db_user, $db_pass)
+		->getRole($AuthorizedUsername, $AuthorizationLevel)
+		->getAuthorizedFields($AuthorizedUsername, $AuthorizedMySQLFields, $AuthorizedLDAPFields)
+		->getUsername($Person->id, $username)
+		->getUser($Person->id, $AuthorizedMySQLFields, $MySQLRecord)
+		->getHistory($Person->id, $SupportHistory)
+		->checkMSEligibility($Person->id, $EligibleForSoftwareCheckout)
+		->getAttributes($PersonalAttributes)
+		->disconnect();
 
 
-   $LDAP
-      ->connect($ldap_user, $ldap_pass)
-      ->getUser($username, $AuthorizedLDAPFields, $LDAPRecord)
-      ->disconnect();
+	$LDAP
+		->connect($ldap_user, $ldap_pass)
+		->getUser($username, $AuthorizedLDAPFields, $LDAPRecord)
+		->disconnect();
 
 
 
@@ -69,11 +69,10 @@ require_once 'php/LDAP.class.php';
 //  Import raw data into the Person object
 // ----------------------------------------
 
-   $Person
-      ->importCategories($PersonalAttributes)
-      ->importLdapData($LDAPRecord)
-      ->importMysqlData($MySQLRecord);
-
+	$Person
+		->importCategories($PersonalAttributes)
+		->importLdapData($LDAPRecord)
+		->importMysqlData($MySQLRecord);
 
 
 
@@ -81,11 +80,11 @@ require_once 'php/LDAP.class.php';
 //  Display data on a webpage
 // ----------------------------------------
 
-   $Person->draw();
+	$Person->draw();
 
-   if ($AuthorizationLevel === 'library')
-      $Person->DisplayMSSoftwareEligibility($EligibleForSoftwareCheckout);
-   else
-      $Person->drawHistory($SupportHistory);
+	if ($AuthorizationLevel === 'library')
+		$Person->DisplayMSSoftwareEligibility($EligibleForSoftwareCheckout);
+	else
+		$Person->drawHistory($SupportHistory);
 
 ?>
