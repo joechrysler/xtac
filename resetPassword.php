47efcc811cd3426fc050a8b33330c2c5ef8b6f0a
Joe Chrysler <mail@joechrysler.com>
2010-05-25 14:24:31 -0400

    removed sanitize include

--- resetPassword.php
+++ resetPassword.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once 'config.php';	// Configuration script
-require_once '/srv/www/live/webapps/include/sanitize.php';	// Provides sanitize_ldap_string()
 
 $ldc = new LDAP($ldap_url);
 
