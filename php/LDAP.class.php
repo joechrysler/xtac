c0f1533d8275cdb4db0f771d4f545b86b47e8b24
Joe Chrysler <mail@joechrysler.com>
2010-05-25 14:44:03 -0400

    added password generator to LDAP

--- php/LDAP.class.php
+++ php/LDAP.class.php
@@ -61,7 +61,7 @@ class LDAP extends XtacData {
 
 		if ($info['count'] === 1) {
 			$userdn = $info[0]['dn'];
-			$newPass = generatePassword();
+			$newPass = $this->generatePassword();
 
 			// Prepare updates:
 			$update['userPassword'] = $newpass;
@@ -122,5 +122,34 @@ class LDAP extends XtacData {
 			false;
 	}
 	
+
+	private function generatePassword($length = 8) {
+		// start with a blank password
+		$password = "";
+
+		// define possible characters
+		$possible = "0123456789bcdfghjkmnpqrstvwxyz"; 
+
+		// set up a counter
+		$i = 0; 
+
+		// add random characters to $password until $length is reached
+		while ($i < $length) { 
+
+			// pick a random character from the possible ones
+			$char = substr($possible, mt_rand(0, strlen($possible)-1), 1);
+
+			// we don't want this character if it's already in the password
+			if (!strstr($password, $char)) { 
+				$password .= $char;
+				$i++;
+			}
+
+		}
+
+		// done!
+		return $password;
+
+	}
 }
 ?>
