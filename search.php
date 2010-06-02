0aa8f81a4dfc9601a979867906d91e5f3d7640bc
Joe Chrysler <mail@joechrysler.com>
2010-06-02 09:51:59 -0400

    Replaced non-semantic variable names

--- search.php
+++ search.php
@@ -3,19 +3,20 @@
 require_once 'php/DataBase.class.php';
 require_once 'config.php';
 
-$db = new DataBase($db_host, $db_name);
+$MySQLDatabase = new DataBase($MySQLDatabase_host, $MySQLDatabase_name);
 $role = '';
 $results = array();
 
 // Check for missing parameters
 if (!@$_GET['q']){
-   echo 'this must be called with a get variable \'q\'';
-   return;
+	echo 'this must be called with a get variable \'q\'';
+	return;
 }
 
-$db->connect($db_user, $db_pass)
-   ->getRole($_SERVER['PHP_AUTH_USER'], $role)
-   ->searchUsers($_GET['q'], $results);
+$MySQLDatabase
+	->connect($MySQLDatabase_user, $MySQLDatabase_pass)
+	->getRole($_SERVER['PHP_AUTH_USER'], $role)
+	->searchUsers($_GET['q'], $results);
 
 echo json_encode($results);
 ?>
