d04bc79631f98fc824e70639ea3d708dedbe316d
Joe Chrysler <mail@joechrysler.com>
2010-09-28 13:59:43 -0400

    Remove useless javascript files

--- js/addfield.js
+++ js/addfield.js
@@ -18,4 +18,33 @@ $(function () {
 			}
 		});
 	});
+
+	$('.button')
+		.mousedown(function () {$(this).addClass('activeButton');})
+		.mouseup(function () {$(this).removeClass('activeButton');})
+		.mouseleave(function () {$(this).removeClass('activeButton');});
+
+	$('#addField').submit(function () {
+
+		var cName = $('#canonicalName').val();
+		var rName = $('#readableName').val();
+		var cat = $('#category').val();
+		var mField = $('#mysqlField').val();
+		var lField = $('#ldapField').val();
+		var authReq = $('#authorizationLevelRequired').val();
+
+		$.post('addField.php', {
+			canonicalName: cName,
+			readableName: rName,
+			category: cat,
+			mysqlField: mField,
+			ldapField: lField,
+			authorizationLevelRequired: authReq},
+			
+			function (data) {
+				$('#success').show('fast');
+			};);
+			
+		return false;
+	});
 });
