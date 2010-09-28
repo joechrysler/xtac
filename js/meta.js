f3b6fbe0099622da221dae5f32a09ded2b2f41e9
Joe Chrysler <mail@joechrysler.com>
2010-09-28 13:36:19 -0400

    Add ajax interception of form submit

--- js/meta.js
+++ js/meta.js
@@ -3,4 +3,25 @@ $().ready(function () {
 		.mousedown(function () {$(this).addClass('activeButton');})
 		.mouseup(function () {$(this).removeClass('activeButton');})
 		.mouseleave(function () {$(this).removeClass('activeButton');});
+
+	$('#submit').submit(function () {
+
+		var cName = $('#canonicalName').val();
+		var rName = $('#readableName').val();
+		var cat = $('#category').val();
+		var mField = $('#mysqlField').val();
+		var lField = $('#ldapField').val();
+		var authReq = $('#authorizationLevelRequired').val();
+
+		$.post('addField.php', {
+			canonicalName: cName,
+			readableName: rName,
+			category: cat,
+			mysqlField: mField,
+			ldapField: lField,
+			authorizationLevelRequired: authReq},
+			function() {return false;}});
+			
+		return false;
+	});
 });
