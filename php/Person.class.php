cda55b065d775a44c3d500b3879fa573356d832c
Joe Chrysler <mail@joechrysler.com>
2010-09-30 09:14:22 -0400

    Add test that prints html classes for unix uid and gid

--- php/Person.class.php
+++ php/Person.class.php
@@ -200,6 +200,9 @@ class Person
 			$this->categories['status']->attributeList['unixUidNum']->LdapValue = 'T';
 			$this->categories['status']->attributeList['unixGidNum']->LdapValue = 'T';
 			$this->categories['status']->attributeList['unixUidNum']->HtmlClass[] = 'error';
+			$this->categories['status']->attributeList['unixGidNum']->HtmlClass[] = 'error';
+			print_nice($this->categories['status']->attributeList['unixUidNum']->HtmlClass);
+			print_nice($this->categories['status']->attributeList['unixGidNum']->HtmlClass);
 			}
 		else {
 			$this->categories['status']->attributeList['unixUidNum']->LdapValue = 'F';
