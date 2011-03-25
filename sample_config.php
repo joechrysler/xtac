ec13745a551617aa2a6d0cf02c023487eace3072
Joseph Chysler <jdchrysl@svsu.edu>
2011-03-25 12:54:47 -0400

    Add sample config file

---
+++ sample_config.php
@@ -0,0 +1,25 @@
+<?php
+
+// Database Host  --  mysql
+//==============================+
+  $db_host        = 'localhost';
+  $db_name        = 'the mysql database you are checking';
+  $db_pass        = 'your mysql password';
+  $db_user        = 'username you connect with';
+  $fieldsDBname   = 'database with your field mappings';
+
+// LDAP Host
+//==============================+
+  $base           = 'your base organization unit, like o=university';
+  $ldap_host      = 'ldap.youruniversity.edu';
+  $ldap_user      = 'cn=UserToBindWith,ou=unit,o=university';
+  $ldap_pass      = 'ldap bind password';
+
+  $pw_user        = 'ldap user with password reset permission';
+  $pw_pass        = 'their password';
+
+// Settings  --  other stuff
+//==============================+
+  $HistoryItemsShown  = 5;
+
+?>
